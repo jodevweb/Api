f5002fd8162f7b4ae9c2ec40102ceb6224c38fa2
jodevweb <jordan_usoulet@live.fr>
2016-05-23 22:08:29 +0200

    up script exemple

--- getJson.php
+++ getJson.php
@@ -1,16 +1,20 @@
 <?php
-/*// GET
-$json = file_get_contents('http://localhost/api/Api/view/articles/apikey/d8ac17509ccf43188b7fdfed9c1b283a');
-
-$response = json_decode($json, true);
-
+// GET
+$ch = curl_init();
+curl_setopt($ch, CURLOPT_URL, "http://localhost/api/Api/view/articles/apikey/d8ac17509ccf43188b7fdfed9c1b283a");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+$output = curl_exec($ch);
+$response = json_decode($output, true);
 if (empty($response->error)) {
     foreach ($response as $row) {
         echo 'Titre: ' . $row['titre'] . '<br>';
     }
 } else {
     echo 'erreur';
-}*/
+}
+curl_close($ch);
+
+
 
 // POST
 $url = 'http://localhost/api/Api/add';
@@ -26,19 +30,10 @@ $fields = array(
     )
 );
 
-//url-ify the data for the POST
 $fields_string = http_build_query($fields);
-
-//open connection
 $ch = curl_init();
-
-//set the url, number of POST vars, POST data
 curl_setopt($ch,CURLOPT_URL, $url);
 curl_setopt($ch,CURLOPT_POST, count($fields));
 curl_setopt($ch,CURLOPT_POSTFIELDS, $fields_string);
-
-//execute post
-$result = curl_exec($ch);
-
-//close connection
+$output = curl_exec($ch);
 curl_close($ch);
